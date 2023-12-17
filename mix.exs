0aa5eb52b83aa0aac8f7c9494bce13c0542cead9
sndnv <sndnv@users.noreply.github.com>
2023-12-17 22:07:09 +0000

    Updating version to 1.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Asciichart.MixProject do
   def project do
     [
       app: :asciichart,
-      version: "1.1.1-SNAPSHOT",
+      version: "1.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       package: package(),
